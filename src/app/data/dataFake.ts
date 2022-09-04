6b73d58971d2e788482fc05def27b4e40b39faed
timzejr2 <timzejr2@gmail.com>
2022-09-04 15:14:09 -0300

    Construção do blog e primeiras postagens

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Angular",
+        "description": "Uma caixa de ferramentas poderosa que permite a criação e montagem de componentes para páginas e diversas formas de interação. Este blog foi construido com Angular!",
+        "photoCover": "../../assets/Angular.png",
+        "linkName": "repositório github - modelo de blog feito em angular",
+        "linkMaterial": "https://github.com/timzejr2/modelo-blog-angular.git"
+    },
+    {
+        "id": "2",
+        "title": "APIs .NET",
+        "description": "Através do entity framework, é possível facilmente se conectar a um banco de dados e gerar as tabelas conforme as classes do programa, e criar as interações CRUD que sua aplicação precisar.",
+        "photoCover": "../../assets/APIdotnet.jpg",
+        "linkName": "repositório github - modelo de API para cadastro de funcionário em banco de dados e histórico de operações com logs",
+        "linkMaterial": "https://github.com/timzejr2/trilha-net-api-desafio.git"
+    },
+    {
+        "id": "3",
+        "title": "Recursos Azure",
+        "description": "Com Azure é possível criar toda estrutura necessária para o funcionamento de uma aplicação, desde bancos de dados, containeres para armazenar arquivos como fotos e vídeos, tabelas para histórico de logos das operações realizadas e a hospedagem da própria aplicação e tudo isso com a opção de pagar apenas os recursos usados pela aplicação.",
+        "photoCover": "../../assets/Azure.png",
+        "linkName": "",
+        "linkMaterial": ""
+    },
+    {
+        "id": "4",
+        "title": "Novo game",
+        "description": "Ainda em produção e sem ter o nome divulgado, este game propõe algumas mudanças durante o game play, como mudar de mundo aberto para plataforma de maneira fluida",
+        "photoCover": "../../assets/newgame.jpg",
+        "linkName": "Confira a gameplay",
+        "linkMaterial": "https://www.youtube.com/watch?v=hTscYk7zs8g&t=3s"
+    },
+    {
+        "id": "5",
+        "title": "Faustera game",
+        "description": "Inspirado em um quadro clássico da televião, sua missão é não deixar ninguém atravessar a ponte",
+        "photoCover": "../../assets/fausteragame.jpg",
+        "linkName": "Confira o game",
+        "linkMaterial": "https://simmer.io/@timzejr/fausteragame"
+    }
+]
